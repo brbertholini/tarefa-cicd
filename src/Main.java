0a0573e571ae7c2930406d78ba9c75c34aaf139e
Bruno Bertholini <brbertholini@gmail.com>
2024-03-23 10:31:50 -0300

    atividade iec

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um número:");
+        int numero = scanner.nextInt();
+
+        if (numero % 2 == 0) {
+            System.out.println("O número " + numero + " é par.");
+        } else {
+            System.out.println("O número " + numero + " é impar.");
+        }
+
+        scanner.close();
+    }
+}
